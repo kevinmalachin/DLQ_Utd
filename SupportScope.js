710f7bdc624d814db5238afd55509fe137a8efbe
Kevin Malachin <kevinmalachin@gmail.com>
2024-01-08 13:26:33 +0100

    Appunti

--- SupportScope.js
+++ SupportScope.js
@@ -1,5 +1,4 @@
 // DOM Elements
-
 const textareaList = document.querySelectorAll("textarea");
 const checkButton = document.querySelector(".Check");
 
@@ -10,23 +9,37 @@ checkButton.addEventListener("click", function (e) {
 });
 
 function calcolaDifferenza() {
-  let allAppsTextarea = document.querySelector(".AllApps").value;
-  let supportScopeTextarea = document.querySelector(".SupportScope").value;
+  let allAppsTextarea = document.querySelector(".AllApps").value.toLowerCase();
+  let supportScopeTextarea = document
+    .querySelector(".SupportScope")
+    .value.toLowerCase();
   let differenceTextarea = document.querySelector(".Difference");
+  let totalAppsCountElement = document.querySelector(".Total-Apps-Count");
 
-  // Converts text in array
-  let allAppsList = allAppsTextarea.split("\n");
+  // Extract only lines that contain app names from the first textarea
+  let allAppsList = allAppsTextarea
+    .split("\n")
+    .map((line) => line.trim().toLowerCase());
+
   let supportScopeList = supportScopeTextarea.split("\n");
 
-  // Difference between the two
-  let differenza = allAppsList.filter((app) => !supportScopeList.includes(app));
+  // Find names present in the second textarea but not in the first
+  let differenza = [];
+  supportScopeList.forEach((app) => {
+    let formattedSupportApp = app.trim().toLowerCase();
+    if (!allAppsList.includes(formattedSupportApp)) {
+      differenza.push(formattedSupportApp);
+    }
+  });
 
-  // Here is the result
+  // Update the result in the third textarea
   differenceTextarea.value = differenza.join("\n");
+
+  // Display the total count of apps in the third textarea
+  totalAppsCountElement.textContent = `Totale app non presenti: ${differenza.length}`;
 }
 
 // Remove placeholder when you click in the textarea
-
 textareaList.forEach(function (textarea) {
   textarea.addEventListener("click", function () {
     this.removeAttribute("placeholder");
