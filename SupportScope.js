d29b1a73099945e05530d2afdefd23c1e78027af
Kevin Malachin <kevinmalachin@gmail.com>
2024-03-25 16:57:59 +0100

    Appunti

--- SupportScope.js
+++ SupportScope.js
@@ -13,8 +13,10 @@ checkButton.addEventListener("click", function (e) {
 // Function to format the input text
 function formatInputText(textareaValue) {
   // Extract only lines starting with the specified format and excluding dates
-  return textareaValue.match(/^\w{4}-\w+.*$/gm)
-    .filter(line => !/\d{4}-\d{2}-\d{2}/.test(line)) || [];
+  const matches = textareaValue.match(/^\w{4}-\w+.*$/gm);
+  return matches
+    ? matches.filter((line) => !/\d{4}-\d{2}-\d{2}/.test(line))
+    : [];
 }
 
 // Function to calculate the difference between texts
@@ -30,7 +32,9 @@ function calculateDifference() {
   const wordsInSecondTextarea = formatInputText(supportScopeText);
 
   // Find words in the first textarea that are not present in the second one
-  const missingWords = wordsInFirstTextarea.filter(word => !wordsInSecondTextarea.includes(word));
+  const missingWords = wordsInFirstTextarea.filter(
+    (word) => !wordsInSecondTextarea.includes(word)
+  );
 
   // Update the result in the third textarea
   differenceTextarea.value = `Apps not found:\n\n${missingWords.join("\n")}`;
