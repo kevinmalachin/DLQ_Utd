a544a9d1b2034d4a50016a5db915a78a9071cc5d
Kevin Malachin <kevinmalachin@gmail.com>
2024-03-26 01:32:45 +0100

    Appunti

--- README.md
+++ README.md
@@ -11,10 +11,8 @@ Support scope check
 ctrl+a to take all the runtime manager content
 copy in the first textarea
 take the column from the Support scope with all the application and paste in the second textarea
-check
-
-in the third text area will appear those apps that are in the runtime (1) but not in the support (2)
-
-of course it will depend on how you filter the support scope
+click "check" -> the button has a function to check with a RegExp if an application is found in the first TextArea (Runtime) but NOT in the second one will appear in the third one.
+So in the third text area will appear those apps that are in the runtime (1) but not in the support (2).
+To avoid the dates (format YYYY-MM-DD) to appear in the third one the RegExp has a special filter that excludes the date format.
 
 STILL TESTING
--- SupportScope.js
+++ SupportScope.js
@@ -3,6 +3,14 @@ const checkButton = document.getElementById("checkButton");
 const allAppsTextarea = document.getElementById("allAppsTextarea");
 const supportScopeTextarea = document.getElementById("supportScopeTextarea");
 const differenceTextarea = document.getElementById("differenceTextarea");
+const menuButton = document.getElementById("menuButton");
+const menu = document.getElementById("menu");
+
+/* // Toggle submenu visibility when menu button is clicked
+menuButton.addEventListener("click", function() {
+  const submenu = menu.querySelector(".submenu");
+  submenu.classList.toggle("show");
+}); */
 
 // Event listener for the check button
 checkButton.addEventListener("click", function (e) {
@@ -18,6 +26,31 @@ function formatInputText(textareaValue) {
     ? matches.filter((line) => !/\d{4}-\d{2}-\d{2}/.test(line))
     : [];
 }
+
+/* // Function to clean the input text
+function cleanText(text) {
+  // Lista di parole da eliminare
+  const paroleDaEliminare = ["CloudHub", "4.4.0", "None", "Started"];
+
+  // Espressione regolare per gli orari e le date
+  const regexDate = /\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}/g;
+
+  // Rimuove gli orari e le date
+  text = text.replace(regexDate, '');
+
+  // Rimuove le parole dalla lista
+  paroleDaEliminare.forEach(parola => {
+    text = text.replace(new RegExp(parola, 'gi'), '');
+  });
+
+  // Rimuove spaziature multiple
+  text = text.replace(/\s+/g, ' ');
+
+  // Rimuove spaziature iniziali e finali
+  text = text.trim();
+
+  return text;
+} */
 
 // Function to calculate the difference between texts
 function calculateDifference() {
@@ -45,5 +78,12 @@ const textareaList = document.querySelectorAll("textarea");
 textareaList.forEach(function (textarea) {
   textarea.addEventListener("click", function () {
     this.removeAttribute("placeholder");
+  });
+});
+
+
+$(document).ready(function () {
+  $("#menuButton").click(function () {
+    $("#menu").slideToggle();
   });
 });
