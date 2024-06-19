edd982a798ca1a6aa2402dfbbf584a86d9909984
Kevin Malachin <kevinmalachin@gmail.com>
2024-06-19 12:45:11 +0200

    DLQ check minor fix, css update and reference retrieve

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const DLQtext = document.querySelector(".DLQtext");
-const incidents = document.querySelector(".Incidents");
 const results = document.querySelector(".Results");
 const check = document.querySelector(".Check");
 const textareaList = document.querySelectorAll("textarea");
@@ -11,8 +10,6 @@ check.addEventListener("click", (e) => {
 
   // take the value from the first text area and store in a variable
   const dlqText = DLQtext.value;
-  // idem for the incident textarea
-  const incidentsText = incidents.value;
 
   // Regex to find "externalReference": and catch what follows
   const externalReferencesInDLQ = Array.from(
@@ -20,16 +17,8 @@ check.addEventListener("click", (e) => {
     (match) => match[1]
   );
 
-  // Split the reference in the incident
-  const referencesInIncidents = incidentsText.split(/\s+/);
-
-  // Find the difference between the two lists of reference
-  const difference = externalReferencesInDLQ.filter(
-    (reference) => !referencesInIncidents.includes(reference)
-  );
-
   // Send the results in the third textarea
-  results.textContent = difference.join(", ");
+  results.textContent = externalReferencesInDLQ.join(", ");
 });
 
 // remove the placeholder when you click
