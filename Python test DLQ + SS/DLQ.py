c2d3ba8e5165a40e1e67e06ebb475e09d2799c0c
Kevin Malachin <kevinmalachin@gmail.com>
2024-08-22 19:40:18 +0200

    almost there

--- Python test DLQ + SS/DLQ.py
+++ Python test DLQ + SS/DLQ.py
@@ -7,6 +7,8 @@ def extract_filtered_references(text):
     cm_pattern = r'\bCM_[\w-]+\b'
     ec_pattern_full = r'\bEC0\d{5}_[\w]+\b'
     ec_pattern_simple = r'\bEC0\d{5}\b(?!_)'  # EC0 seguito da numeri e NON seguito da "_"
+    # RegEx per catturare asnType e asnId insieme
+    asn_pattern = r'"asnType"\s*:\s*"(\w+)"\s*,\s*"asnId"\s*:\s*"(\d+)"'
 
     # Trova tutte le reference
     fr_matches = re.findall(fr_pattern, text)
@@ -14,10 +16,15 @@ def extract_filtered_references(text):
     cm_matches = re.findall(cm_pattern, text)
     ec_matches_full = re.findall(ec_pattern_full, text)
     ec_matches_simple = re.findall(ec_pattern_simple, text)
+    asn_matches = re.findall(asn_pattern, text)  # Trova gli asnType e asnId
 
     # Aggrega tutte le reference rimuovendo i duplicati
     all_references = list(set(fr_matches + zeco_matches + cm_matches +
                               ec_matches_full + ec_matches_simple))
+
+    # Aggiungi gli asnId con il relativo asnType al risultato
+    for asn_type, asn_id in asn_matches:
+        all_references.append(f"{asn_id} [{asn_type}]")
 
     # Prepara l'output
     output_text = f"References found: {len(all_references)}\n"
