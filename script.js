0381f3b894dfadcfd4f1b9be2d11dcdf0a01c0ff
Kevin Malachin <kevinmalachin@gmail.com>
2024-10-01 23:30:54 +0200

    goods fixed for internal reference

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const menu = document.querySelector("#menu");
 
 if (menuButton && menu) {
     menuButton.addEventListener("click", () => {
-        menu.classList.toggle("hidden"); // Aggiunge o rimuove la classe 'hidden' per nascondere/mostrare il menu
+        menu.classList.toggle("hidden");
     });
 } else {
     console.error("Menu or Menu Button not found in the page.");
@@ -46,102 +46,54 @@ if (!DLQtext || !results || !extractButton || !checkButton) {
                 patterns = [/\"ref\":\s*\"(CM_[^\"]+)\"/g];
                 break;
             case /emea\.eboutique\.deposit\.cancel/.test(currentDLQ):
-                patterns = [/\"creditMemoReference\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
+                patterns = [/\"creditMemoReference\":\s*\"([^\"]+)\"/g];
                 break;
             case /emea\.eboutique\.return-notices/.test(currentDLQ):
-                patterns = [/\"externalReference\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
+                patterns = [/\"externalReference\":\s*\"([^\"]+)\"/g];
                 break;
             case /emea\.orderlifecycle\.sendmailccreminder2/.test(currentDLQ):
-                patterns = [/\"rootEntityRef\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
-                break;
             case /emea\.orderlifecycle\.sendmailccreminder1/.test(currentDLQ):
-                patterns = [/\"rootEntityRef\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
+                patterns = [/\"rootEntityRef\":\s*\"([^\"]+)\"/g];
                 break;
             case /emea\.orderlifecycle\.SendASN/.test(currentDLQ):
-                patterns = [/\"entityRef\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
+                patterns = [/\"entityRef\":\s*\"([^\"]+)\"/g];
                 break;
             case /emea\.orderlifecycle\.cdc-route/.test(currentDLQ):
-                patterns = [/\"internalReference\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
+                patterns = [/\"internalReference\":\s*\"([^\"]+)\"/g];
                 break;
             case /emea\.orderlifecycle\.returnreshipped/.test(currentDLQ):
-                patterns = [/\"rootEntityRef\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
+                patterns = [/\"rootEntityRef\":\s*\"([^\"]+)\"/g];
                 break;
             case /emea\.orderlifecycle\.paymentReversals/.test(currentDLQ):
-                patterns = [/\"entityRef\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
+                patterns = [/\"entityRef\":\s*\"([^\"]+)\"/g];
                 break;
             case /emea\.orderlifecycle\.cscrtsalert/.test(currentDLQ):
-                patterns = [/\"rootEntityRef\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
+                patterns = [/\"rootEntityRef\":\s*\"([^\"]+)\"/g];
                 break;
-                case /prod\.emea\.orderlifecycle\.alf-route/.test(currentDLQ):
-                    patterns = [
-                        /\"externalReference\":\s*\"([^\"]+)\"/g, 
-                        /\"internalReference\":\s*\"([^\"]+)\"/g,
-                        /\"entityId\":\s*\"([^\"]+)\"/g,
-                        /\"storeCode\":\s*\"([^\"]+)\"/g,
-                        /\"orderCountryCode\":\s*\"([^\"]+)\"/g
-                    ];
-                    break;
+            case /prod\.emea\.orderlifecycle\.alf-route/.test(currentDLQ):
+                patterns = [
+                    /\"externalReference\":\s*\"([^\"]+)\"/g, 
+                    /\"internalReference\":\s*\"([^\"]+)\"/g,
+                    /\"entityId\":\s*\"([^\"]+)\"/g,
+                    /\"storeCode\":\s*\"([^\"]+)\"/g,
+                    /\"orderCountryCode\":\s*\"([^\"]+)\"/g
+                ];
+                break;
             case /apac\.supply\.notifications\.transfer/.test(currentDLQ):
-                patterns = [/\"Number\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
+                patterns = [/\"Number\":\s*\"([^\"]+)\"/g];
                 break;
-            case /emea\.orderFromStore\.availableCustomerOrders\.sac/.test(currentDLQ):
-                patterns = [/\"internalReference\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
-                break;
-            case /prod\.emea\.store-factory\.orderFromStore\.availableCustomerOrders\.sac/.test(currentDLQ):
-                patterns = [/\"internalReference\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
-                break;
-            case /prod\.amer\.store-factory\.orderFromStore\.availableCustomerOrders\.sac/.test(currentDLQ):
-            patterns = [/\"internalReference\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
-                break;
-            case /prod\.apac\.store-factory\.orderFromStore\.availableCustomerOrders\.sac/.test(currentDLQ):
-            patterns = [/\"internalReference\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
-                break;
-            case /apac\.orderFromStore\.availableCustomerOrders\.sac/.test(currentDLQ):
-                patterns = [/\"internalReference\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
+            case /process\.goods-receptions/.test(currentDLQ):
+                patterns = [
+                    /\"asnType\":\s*\"([A-Z]+)\"/g, 
+                    /\"asnId\":\s*\"(\d+)\"/g,
+                    /\"asnInternalReference\":\s*\"(\d+)\"/g
+                ];
                 break;
             case /orderlifecycle\.sendpartialrefund/.test(currentDLQ):
                 patterns = [/\"entityRef\":\s*\"(CM_[^\"]+)\"/g];
                 break;
-            case /process\.goods-receptions/.test(currentDLQ):
-                patterns = [/\"asnType\":\s*\"([A-Z]+)\"/g, /\"asnId\":\s*\"(\d+)\"/g];
-                break;
-            case /process\.generateinvoice/.test(currentDLQ):
-                patterns = [/\"internalReference\":\s*\"(EC0[^\"]+)\"/g];
-                break;
-            case /orderlifecycle\.LTReserveFulfilment/.test(currentDLQ):
-            case /orderlifecycle\.LTRejectFulfilment/.test(currentDLQ):
-            case /orderlifecycle\.LTValidateFulfilment/.test(currentDLQ):
-                patterns = [/\"rootEntityRef\":\s*\"(FR\d+|EC\d+)\"/g];
-                break;
-            case /emea\.orderlifecycle\.createLabelSAV/.test(currentDLQ):
-            case /orderlifecycle\.sendcodrefundcase/.test(currentDLQ):
-                patterns = [/\"entityRef\":\s*\"(EC\d+-R\d+)\"/g];
-                break;
-            case /emea\.m51au\.process/.test(currentDLQ):
-            case /apac\.orderlifecycle\.dhl\.kr\.delivery/.test(currentDLQ): 
-                patterns = [/\"REFLIV\":\s*\"(EC\d+-\d+)\"/g];
-                break;
-            case /prod\.emea\.orderlifecycle\.OrderCreation/.test(currentDLQ):
-                // Updated pattern specifically for `rootEntityRef`
-                patterns = [/\"rootEntityRef\":\s*\"(EC\d+)\"/g];
-                break;
-            case /emea\.orderlifecycle\.fullordercancellation/.test(currentDLQ):
-                patterns = [/\"entityRef\":\s*\"(EC\d+)\"/g];
-                break;
-            case /prod\.emea\.orderlifecycle\.sendmailccreminder1/.test(currentDLQ):
-                patterns = [/\"entityRef\":\s*\"(EC\d+)\"/g];
-                break;
-            case /prod\.emea\.eboutique\.order/.test(currentDLQ):
-                patterns = [/\"externalReference\":\s*\"(EC\d+)\"/g];
-                break;
             case /prod\.emea\.storeFactory\.orderFromStore\.sales/.test(currentDLQ):
-                patterns = [/\"internalReference\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
-                break;
-            case /prod\.emea\.orderlifecycle\.GenerateInvoice/.test(currentDLQ):
-                patterns = [/\"rootEntityRef\":\s*\"([^\"]+)\"/g]; // Aggiunta del nuovo pattern per catturare `rootEntityRef`
-                break;
-            case /prod\.emea\.orex\.financial-transactions-creation/.test(currentDLQ):
-                patterns = [/\"orderRef\":\s*\"([^\"]+)\"/g]; // Cattura qualsiasi valore tra virgolette
+                patterns = [/\"internalReference\":\s*\"([^\"]+)\"/g];
                 break;
             default:
                 console.error("No matching DLQ pattern found.");
@@ -152,20 +104,16 @@ if (!DLQtext || !results || !extractButton || !checkButton) {
         // Logica per combinare riferimenti specifici (come alf-route e goods-receptions)
         let combinedReferences = [];
         if (currentDLQ.includes("goods-receptions")) {
-            // Gestione specifica per goods-receptions
             const asnTypeMatches = [...dlqText.matchAll(/\"asnType\":\s*\"([A-Z]+)\"/g)];
             const asnIdMatches = [...dlqText.matchAll(/\"asnId\":\s*\"(\d+)\"/g)];
+            const asnInternalRefMatches = [...dlqText.matchAll(/\"asnInternalReference\":\s*\"(\d+)\"/g)];
 
-            if (asnIdMatches.length > 0) {
-                for (let i = 0; i < asnIdMatches.length; i++) {
-                    const asnType = asnTypeMatches[i] ? asnTypeMatches[i][1] : "asnType not found";
-                    combinedReferences.push(`${asnIdMatches[i][1]} [${asnType}]`);
-                }
-            } else {
-                combinedReferences = asnIdMatches.map(match => `${match[1]} [asnType not found]`);
+            for (let i = 0; i < asnIdMatches.length; i++) {
+                const asnType = asnTypeMatches[i] ? asnTypeMatches[i][1] : "asnType not found";
+                const asnInternalReference = asnInternalRefMatches[i] ? asnInternalRefMatches[i][1] : "asnInternalReference not found";
+                combinedReferences.push(`${asnIdMatches[i][1]} [${asnType}, ${asnInternalReference}]`);
             }
         } else if (currentDLQ.includes("alf-route")) {
-            // Gestione specifica per alf-route
             const externalReferences = [...dlqText.matchAll(/\"externalReference\":\s*\"([^\"]+)\"/g)];
             const internalReferences = [...dlqText.matchAll(/\"internalReference\":\s*\"([^\"]+)\"/g)];
             const entityIds = [...dlqText.matchAll(/\"entityId\":\s*\"([^\"]+)\"/g)];
@@ -182,7 +130,6 @@ if (!DLQtext || !results || !extractButton || !checkButton) {
                 combinedReferences.push(`${externalReference} [${internalReference}, ${entityId}, ${storeCode}, ${orderCountryCode}]`);
             }
         } else {
-            // Default: Cattura qualsiasi altro pattern definito
             patterns.forEach((pattern) => {
                 const matches = [...dlqText.matchAll(pattern)];
                 combinedReferences.push(...matches.map((match) => match[1]));
@@ -211,7 +158,8 @@ if (!DLQtext || !results || !extractButton || !checkButton) {
             const response = await fetch("http://localhost:5000/run-script", {
                 method: "POST",
                 headers: {
-                    "Content-Type": "application/json"},
+                    "Content-Type": "application/json"
+                },
                 body: JSON.stringify({ references: extractedReferences }),
             });
 
@@ -226,24 +174,20 @@ if (!DLQtext || !results || !extractButton || !checkButton) {
             }
             reportedRefs.forEach((ref) => nonReportedRefs.delete(ref));
 
-            // Conta totale delle referenze
             const totalReferencesCount = extractedReferences.length;
             const reportedCount = reportedRefs.size;
             const nonReportedCount = nonReportedRefs.size;
 
-            // Output delle referenze totali, riportate e non riportate
             let totalReferencesCountText = `<p><strong>Total References Found:</strong> ${totalReferencesCount}</p>`;
             totalReferencesCountText += `<p><strong>Reported References:</strong> ${reportedCount}</p>`;
             totalReferencesCountText += `<p><strong>Non-reported References:</strong> ${nonReportedCount}</p>`;
 
-            // Mostra referenze non riportate in rosso grassetto
             let nonReportedText = `<p style="color:red;"><strong>Non-reported References (${nonReportedCount}):</strong></p><ul>`;
             nonReportedRefs.forEach((ref) => {
                 nonReportedText += `<li style="color:red;"><strong>${ref}</strong></li>`;
             });
             nonReportedText += "</ul>";
 
-            // Mostra referenze riportate con i dettagli della task in verde grassetto
             let reportedText = `<p style="color:green;"><strong>Reported References (${reportedCount}):</strong></p><ul>`;
             for (const [incident, details] of Object.entries(output.reported)) {
                 if (details.references_count > 0) {
@@ -257,7 +201,6 @@ if (!DLQtext || !results || !extractButton || !checkButton) {
             }
             reportedText += "</ul>";
 
-            // Unisci il tutto nel div dei risultati
             results.innerHTML = totalReferencesCountText + reportedText + nonReportedText;
         } catch (error) {
             console.error(error);
